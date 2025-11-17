dc96ba8b1a93ced3ae0a46ec90fe9cefaac0091f
AshvinJoseph2 <ashvinj893@gmail.com>
2025-11-17 13:02:56 +0530

    wishlist added

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,48 +1,73 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToWishlist } from '../redux/slices/wishlistSlice'
+import Swal from 'sweetalert2'
 
 function View() {
   // get path parameters from url
-  // const {id}= useParams()
-  // console.log(id);
+  const {id}= useParams()
+  console.log(id);
   // state to store product details
-  // const[product,setProduct]=useState({})
-
+  const[product,setProduct]=useState({})
+  const dispatch = useDispatch()
+  const userWishlist = useSelector(state=>state.wishlistReducer)
   // console.log(product);
   
-  // useEffect(()=>{
-  //   getProductDetails()
-  // },[])
+  useEffect(()=>{
+    getProductDetails()
+  },[])
 
-  // const getProductDetails = ()=>{
-  //   const allproducts = JSON.parse(sessionStorage.getItem("products"))
-  //   setProduct(allProducts.find(item=>item.id=id))
-  // }
+  const getProductDetails = ()=>{
+    const allProducts = JSON.parse(sessionStorage.getItem("products"))
+    setProduct(allProducts.find(item=>item.id==id))
+  }
+
+const handleAddToWishlist = ()=>{
+  const existingProduct = userWishlist?.find(item=>item.id==id)
+  if(existingProduct){
+    Swal.fire({
+  title: 'Sorry!!!',
+  text: 'Product already in your Wishlist...',
+  icon: 'error',
+  confirmButtonText: 'OK'
+  })
+  }else{
+    dispatch(addToWishlist(product))
+  }
+}
+
   return (
     <>
     <Header/>
     <div className='container my-5'>
-      <div  style={{height:"90vh"}} className='row pt-5 align-items-center'>
+      <div  style={{minHeight:"80vh"}} className='row pt-5 align-items-center'>
         <div className='col-md-6 text-center'>
-          <img className='img-fluid' src="https://tse2.mm.bing.net/th/id/OIP.Y2oxV98mPTIwWJG9VbK9sQHaHa?pid=Api&P=0&h=180" alt="products" />
+          <img className='img-fluid' src={product?.thumbnail} alt="products" />
           <div className='d-flex justify-content-evenly mt-5'>
-            <button className='btn btn-info'>ADD TO WISHLIST</button>
+            <button onClick={handleAddToWishlist} className='btn btn-info'>ADD TO WISHLIST</button>
             <button className='btn btn-success'>ADD TO CART</button>
           </div>
         </div>
         <div className='col-md-6'>
-          <h1>title</h1>
-          <h2 className='text-danger'>$ price</h2>
-          <h3 className='mt-2'>Brand :</h3>
-          <h3>Category :</h3>
-          <h4>Descrption :</h4>
+          <h1>{product?.title}</h1>
+          <h2 className='text-danger'>$ {product?.price}</h2>
+          <h3 className='mt-2'>Brand : {product?.brand}</h3>
+          <h3>Category : {product?.category}</h3>
+          <h4>Descrption : {product?.description}</h4>
           <h5 className='fw-bolder my-3'>Client Reviews</h5>
-          <div className='my-5 border rounded shadow p-2'>
-            <p><span className='fw-bold'>Clientname :</span>message</p>
-            <p>Rating : number<FontAwesomeIcon icon={faStar} className='text-warning' /></p>
-          </div>
+         { 
+           product?.reviews?.length>0 &&
+           product?.reviews?.map((item,index)=>(
+             <div key={index} className='my-5 border rounded shadow p-2'>
+               <p><span className='fw-bold'>{item.reviewerName} :</span>{item.comment}</p>
+               <p>Rating : {item.rating}<FontAwesomeIcon icon={faStar} className='text-warning' /></p>
+             </div>
+           )) 
+          }
         </div>
       </div>
     </div>
